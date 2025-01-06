1a94f436f3f1b39cd9e937975200ffda0bc464e4
Akaytab Karmakar <akaytab.karmakar@ing.com>
2025-01-06 10:09:20 +0530

    web application demo project

---
+++ projects/webApplication/src/components/User/User.jsx
@@ -0,0 +1,12 @@
+import { useParams } from "react-router"
+
+function User(){
+    const {userid} =useParams();
+    return(
+        <div>
+        User : {userid}
+        </div>
+    )
+
+}
+export default User
